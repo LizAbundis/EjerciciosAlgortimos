b8f1ebcbb96facd34107dd826270ddd831e09f98
liz <silvia98abundis@hotmail.com>
2022-05-26 10:14:15 -0500

    Primer commit de los ejercicios de Javascript

---
+++ js/main.js
@@ -0,0 +1,90 @@
+       //1. Solicitar 3 números (entre el 1 y el 100) y definir cuál es el menor de tres números
+/*
+let a = prompt ("Ingresa el primer número del 1 al 100","");
+let b = prompt ("Ingresa el segundo número del 1 al 100","");
+let c = prompt ("Ingresa el tercer número del 1 al 100","");
+
+parseFloat=(a); // Es para que nos valide decimales | convierte cadena string en número con val de decimales.
+parseFloat=(b);
+parseFloat=(c);
+
+if((a>b) && (a>c)){
+    console.log(a);
+} else if ((b>c) && (b>a)){
+    console.log(b);
+}else if ((c>b) && (c>a)){
+console.log(c);
+}
+            */   
+//2. Solicitar 3 números ( entre el 1 y el 100) definir cual es menor
+
+/*
+let a = prompt("Ingresa el primer número del 1 al 100","");
+let b = prompt("Ingresa el segundo número del 1 al 100","");
+let c = prompt("Ingresa el tercer número del 1 al 100","");
+
+parseFloat=(a); // Es para que nos valide decimales | convierte cadena string en número con val de decimales.
+parseFloat=(b);
+parseFloat=(c);
+
+if((a<b) && (a<c)){
+    console.log(a);
+}else if ((b<c) && (b<a)){
+    console.log(b);
+}else if ((c<b) && (c<a));{
+console.log(c);
+}
+            */
+       
+               
+    //4.Si es multiplo de 3
+    /*
+    let Liz = prompt("Ingresa un número entero del 100 al 200", "");
+    perseInt= (Liz); //perseInt es para que todo sea entero
+
+    let Silvia= (Liz%3)
+    if ( Silvia == 0 ){// No debe de tener decimales por eso es 0
+console.log(Silvia);//Llamar variable, solo se pone una vez
+        alert("Si es multiplo de 3" , );
+} else if (Silvia != 0 ){
+    alert("No es multiplo de 3");
+} */
+
+            
+
+    
+//Suma de los otros dos números
+   
+            /*
+    let x1 = prompt ("Ingresa el primer número", "");
+    let x2 = prompt ("Ingresa el primer número" ,"");
+    let x3 = prompt ("Ingresa el primer número" , "" );
+
+    x1= parseInt (x1,10);
+    x2= parseInt (x2,10);
+    x3= parseInt (x3,10);
+
+    
+if(( x3== (x1+x2)) )
+{
+    alert( x3 + " es la suma de los otros dos números");
+} else if ((x2== (x1 + x3))){
+    alert(x2+ "es la suma de los otros números");
+
+}
+else if ((x1== (x2 + x3))){
+    alert(x1+ "es la suma de los otros números");
+}
+*/
+
+
+//6.Elaborar un algoritmo para leer un número y determinar si es par o impar
+
+/* let Liz= prompt("Ingresa cualquier número")
+ parseFloat = (Liz)
+
+if(Liz % 2 == 0 ){
+    alert( Liz + "es número par");
+} else {
+    alert(Liz + "es número impar");
+}  */
